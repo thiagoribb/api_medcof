60489ac6189ddaa03773f8778e9f7dd1b28a5076
thiagoribb <thiribeiro142@gmail.com>
2024-03-13 12:02:46 -0300

    feat: config mongodb docker and prisma

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 import app from "./app";
 
 dotenv.config();
 
 const port = process.env.PORT;
+const prisma = new PrismaClient();
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+async function main() {
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$connect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
